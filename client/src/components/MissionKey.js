7dcc76e7f3b575dd97719be48711f32595023b58
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2019-10-29 15:28:57 +0200

    Added Missionkey component

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MissionKey = () => {
+  return (
+    <div className="">
+      <p>
+        <span className="color-marker bg-success"></span> = Success
+      </p>
+      <p>
+        <span className="color-marker bg-danger"></span> = Fail
+      </p>
+    </div>
+  );
+}
+
+export default MissionKey;
